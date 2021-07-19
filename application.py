dee574abb8ca88ed90243662957780fe64dcb712
Ji Kim <jykcs95@gmail.com>
2021-07-19 10:10:40 -0400

    wt

--- application.py
+++ application.py
@@ -1,17 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from incollege import app, routes
-
-application = Flask(__name__)
-app = application
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY']  = 'thisisasecretkey'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+from incollege import app
 if __name__ == "__main__":
     app.run(debug=True)
